253220b2f5b34959f8d073280b1d35e811553377
Yupaporn R. <lyupaporn.r@gmail.com>
2023-12-20 22:47:16 +0700

    feat(states-greeting): add completed exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,34 @@
 import "./App.css";
+import { useState } from "react"; //import function useState จาก React Package เข้ามา
 
 function App() {
+  //ใส่ Argument ตั้งต้นของฟังก์ชัน
+  const [state, setState] = useState("Greeting Message"); // [state, function setState]
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{state}</div>
       <div className="buttons">
-        <button>สวัสดี!</button>
-        <button>Hi!</button>
-        <button>你好!</button>
+        <button
+          onClick={() => {
+            setState("สวัสดี");
+          }}
+        >
+          สวัสดี!
+        </button>
+        <button
+          onClick={() => {
+            setState("Hi!");
+          }}
+        >
+          Hi!
+        </button>
+        <button
+          onClick={() => {
+            setState("你好!");
+          }}
+        >
+          你好!
+        </button>
       </div>
     </div>
   );
